14c6203e597980e14dfb5dfeb893b651fcabdde3
zhanaym21-01 <119833949+zhanaym21-01@users.noreply.github.com>
2022-12-10 14:48:52 +0600

    добавила дз 2

---
+++ DZ2_zhanaiym_21-01/main.js
@@ -0,0 +1,25 @@
+
+var arr = [];
+
+for (i=0;i<=10;i++) {
+document.write(i);
+console.log(arr.push('5'))
+}
+
+for(i=20;i<=0;i--) {
+document.write(i);
+}
+
+
+
+var light = Lighgt(prompt('введите цвет'))
+    if (light === 'red') {
+        alert('стой!')
+    } else if (light === 'yellow') {
+        alert('готовся!')
+    }  else if (light === 'green') {
+        alert('иди!')
+    } else {
+        alert('надо ввести цвет!')
+}
+
